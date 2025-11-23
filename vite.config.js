182942116eb2a6fda8262d08a4c4f172d01d2f82
fuflik52 <klox4462@gmail.com>
2025-11-23 03:33:27 +0300

    Fix: image proxy configuration and server db initialization

--- vite.config.js
+++ vite.config.js
@@ -30,6 +30,11 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false
+      },
+      '/img': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
       }
     }
   }
